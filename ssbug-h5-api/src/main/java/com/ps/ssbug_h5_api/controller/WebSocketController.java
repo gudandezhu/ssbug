f9732ada9edf68197b647cac6a5ff09c157582d0
gudandezhu <1115820867@qq.com>
2019-08-04 08:24:05 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	readme.md

--- ssbug-h5-api/src/main/java/com/ps/ssbug_h5_api/controller/WebSocketController.java
+++ ssbug-h5-api/src/main/java/com/ps/ssbug_h5_api/controller/WebSocketController.java
@@ -1,96 +1,82 @@
 package com.ps.ssbug_h5_api.controller;
 
+import com.ps.ssbug_h5_api.vo.UserSessionVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 
-@ServerEndpoint("/websocket/{sid}")
+@ServerEndpoint("/websocket/{userId}")
 @Component
 @Slf4j
 public class WebSocketController {
 
-    //concurrent包的线程安全map，用来存放每个客户端对应的MyWebSocket对象。
-    private static ConcurrentHashMap<String,Session> map = new ConcurrentHashMap();
 
-    //与某个客户端的连接会话，需要通过它来给客户端发送数据
-    private Session session;
+    //redis存放session
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
 
-    //接收 userId
-    private String userId="";
+    private ListOperations list = redisTemplate.opsForList();
 
 
     /**
-     * 连接建立成功调用的方法*/
+     * 连接建立成功调用的方法
+     */
     @OnOpen
-    public void onOpen(Session session,@PathParam("sid") String userId) {
-        this.session = session;
+    public void onOpen(Session session, @PathParam("userId") String userId) {
+        UserSessionVO uVO = new UserSessionVO();
+        uVO.setUserId(userId);
+        uVO.setSession(session);
 
-        map.put(userId,this.session);     //加入map中
 
-        log.info("有新窗口开始监听:"+userId+",当前在线人数为" + getOnlineCount());
+        list.leftPush("session", uVO);
 
-        this.userId=userId;
+        log.info("有新窗口开始监听:" + userId + ",当前在线人数为" + getOnlineCount());
+
 
         try {
-        	 sendMessage("连接成功");
+            session.getBasicRemote().sendText("连接成功");
         } catch (IOException e) {
             log.error("websocket IO异常");
         }
+
+        log.info("当前用户信息:" + list.range("session", 0, 100));
     }
 
     /**
      * 连接关闭调用的方法
      */
     @OnClose
-    public void onClose() {
+    public void onClose(Session session) throws IOException {
 
-        map.remove(userId);  //从set中删除
+        list.remove("session", 1, session);
 
-        log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
+        log.info("有一连接关闭！当前在线人数为:" + getOnlineCount());
+        log.info("当前用户信息:" + list.range("session", 0, 100));
+
+        session.getBasicRemote().sendText("连接关闭");
     }
 
     /**
-     * 实现服务器主动推送
+     * 接收信息的方法
      */
-    public void sendMessage(String message) throws IOException {
-        this.session.getBasicRemote().sendText(message);
+    @OnMessage
+    public void onMessage(Session session, String msg) {
+        System.out.println(msg);
     }
 
     /**
-     * 向某个人发送消息
-     *    参数 :  对方 id
+     * @param session
+     * @param error
      */
-    public void sendMessageByUserId(String userId , String msg) throws IOException {
-        map.get(userId).getBasicRemote().sendText(msg);
-    }
-
-    /**
-     * 像所有人发送消息
-     *
-     * */
-    public void sendMessageForAll(String msg) throws IOException {
-        Set<Map.Entry<String, Session>> entries = map.entrySet();
-        for (Map.Entry<String, Session> entry : entries) {
-            map.get(entry.getKey()).getBasicRemote().sendText(msg);
-        }
-        log.info("群发了一次消息--------");
-    }
-
-
-
-	/**
-	 * 
-	 * @param session
-	 * @param error
-	 */
     @OnError
     public void onError(Session session, Throwable error) {
         log.error("发生错误");
@@ -98,8 +84,8 @@ public class WebSocketController {
     }
 
 
-    public static synchronized int getOnlineCount() {
-        return map.size();
+    public synchronized Long getOnlineCount() {
+        return list.size("session");
     }
 
 }
